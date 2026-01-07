89e521445a6e4f7655814658b3829745262240c2
Rauan <asetovrauan0@gmail.com>
2026-01-07 11:45:08 +0500

    logger util

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+import logging
+import sys
+
+# Configure logging
+logging.basicConfig(
+    stream=sys.stdout,
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+)
+logger = logging.getLogger("weather_service")
